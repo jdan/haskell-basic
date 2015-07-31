2c1b453abe79022bf070844cbd80454ee89cd3d1
Jordan Scales <scalesjordan@gmail.com>
2015-07-30 19:08:16 -0700

    using where clases for cleanliness

--- basic-parser.hs
+++ basic-parser.hs
@@ -31,54 +31,23 @@ data BasicLine = NumberedLine BasicNumber Statement
                | UnnumberedLine Statement
                deriving (Show)
 
-parseNumberedLine :: Parser BasicLine
-parseNumberedLine = do
-    number <- parseNumber
-    spaces
-    statement <- parseStatement
-    return $ NumberedLine number statement
-
-parseUnnumberedLine :: Parser BasicLine
-parseUnnumberedLine = UnnumberedLine <$> parseStatement
-
+-- LINES
+-- line ::= number statement CR | statement CR
 parseLine :: Parser BasicLine
 parseLine = parseNumberedLine <|> parseUnnumberedLine
+    where
+        parseNumberedLine :: Parser BasicLine
+        parseNumberedLine = do
+            number <- parseNumber
+            spaces
+            statement <- parseStatement
+            return $ NumberedLine number statement
+
+        parseUnnumberedLine :: Parser BasicLine
+        parseUnnumberedLine = UnnumberedLine <$> parseStatement
 
 -- EXPRESSIONS
 -- expression ::= (+|-|ε) term ((+|-) term)*
-parseBareExpression :: Parser Expression
-parseBareExpression = do
-    term <- parseTerm
-    return $ BareExpression term
-
-parseBinaryExpression :: Char -> (Term -> Term -> Expression) -> Parser Expression
-parseBinaryExpression op typeclass = do
-    left <- parseTerm
-    spaces
-    char op
-    spaces
-    right <- parseTerm
-    return $ typeclass left right
-
-parsePlusExpression :: Parser Expression
-parsePlusExpression = parseBinaryExpression '+' PlusExpression
-
-parseMinusExpression :: Parser Expression
-parseMinusExpression = parseBinaryExpression '-' MinusExpression
-
-parseUnaryExpression :: Char -> (Term -> Expression) -> Parser Expression
-parseUnaryExpression op typeclass = do
-    char op
-    spaces
-    term <- parseTerm
-    return $ typeclass term
-
-parseUnaryPlusExpression :: Parser Expression
-parseUnaryPlusExpression = parseUnaryExpression '+' UnaryPlusExpression
-
-parseUnaryMinusExpression :: Parser Expression
-parseUnaryMinusExpression = parseUnaryExpression '-' UnaryMinusExpression
-
 parseExpression :: Parser Expression
 parseExpression =
     try parsePlusExpression <|>
@@ -87,66 +56,102 @@ parseExpression =
     try parseUnaryMinusExpression <|>
     parseBareExpression
 
+    where
+        parseBareExpression :: Parser Expression
+        parseBareExpression = do
+            term <- parseTerm
+            return $ BareExpression term
+
+        parseBinaryExpression :: Char -> (Term -> Term -> Expression) -> Parser Expression
+        parseBinaryExpression op typeclass = do
+            left <- parseTerm
+            spaces
+            char op
+            spaces
+            right <- parseTerm
+            return $ typeclass left right
+
+        parsePlusExpression :: Parser Expression
+        parsePlusExpression = parseBinaryExpression '+' PlusExpression
+
+        parseMinusExpression :: Parser Expression
+        parseMinusExpression = parseBinaryExpression '-' MinusExpression
+
+        parseUnaryExpression :: Char -> (Term -> Expression) -> Parser Expression
+        parseUnaryExpression op typeclass = do
+            char op
+            spaces
+            term <- parseTerm
+            return $ typeclass term
+
+        parseUnaryPlusExpression :: Parser Expression
+        parseUnaryPlusExpression = parseUnaryExpression '+' UnaryPlusExpression
+
+        parseUnaryMinusExpression :: Parser Expression
+        parseUnaryMinusExpression = parseUnaryExpression '-' UnaryMinusExpression
+
 -- FACTORS
 -- factor ::= var | number | (expression)
-parseVarFactor :: Parser Factor
-parseVarFactor = VarFactor <$> parseVar
-
-parseBasicNumberFactor :: Parser Factor
-parseBasicNumberFactor = BasicNumberFactor <$> parseNumber
-
--- Parse an expression factor, or an expression surrounded by parentheses
-parseExpressionFactor :: Parser Factor
-parseExpressionFactor = do
-    char '('
-    expression <- parseExpression
-    char ')'
-    return $ ExpressionFactor expression
-
 parseFactor :: Parser Factor
-parseFactor = parseVarFactor <|> parseBasicNumberFactor <|> parseExpressionFactor
+parseFactor =
+    parseVarFactor <|> parseBasicNumberFactor <|> parseExpressionFactor
+
+    where
+        parseVarFactor :: Parser Factor
+        parseVarFactor = VarFactor <$> parseVar
+
+        parseBasicNumberFactor :: Parser Factor
+        parseBasicNumberFactor = BasicNumberFactor <$> parseNumber
+
+        -- Parse an expression factor, or an expression surrounded by parentheses
+        parseExpressionFactor :: Parser Factor
+        parseExpressionFactor = do
+            char '('
+            expression <- parseExpression
+            char ')'
+            return $ ExpressionFactor expression
 
 -- TERMS
 -- term ::= factor ((*|/) factor)*
-parseBareTerm :: Parser Term
-parseBareTerm = BareTerm <$> parseFactor
-
-parseBinaryTerm :: Char -> (Factor -> Factor -> Term) -> Parser Term
-parseBinaryTerm op typeclass = do
-    left <- parseFactor
-    spaces
-    char op
-    spaces
-    right <- parseFactor
-    return $ typeclass left right
-
-parseMultiplyTerm = parseBinaryTerm '*' MultiplyTerm
-parseDivideTerm = parseBinaryTerm '/' DivideTerm
-
 parseTerm :: Parser Term
 parseTerm = try parseMultiplyTerm <|> try parseDivideTerm <|> parseBareTerm
+    where
+        parseBareTerm :: Parser Term
+        parseBareTerm = BareTerm <$> parseFactor
+
+        parseBinaryTerm :: Char -> (Factor -> Factor -> Term) -> Parser Term
+        parseBinaryTerm op typeclass = do
+            left <- parseFactor
+            spaces
+            char op
+            spaces
+            right <- parseFactor
+            return $ typeclass left right
+
+        parseMultiplyTerm = parseBinaryTerm '*' MultiplyTerm
+        parseDivideTerm = parseBinaryTerm '/' DivideTerm
 
 -- STATEMENTS
-parsePrintStatement :: Parser Statement
-parsePrintStatement = do
-    string "PRINT"
-    spaces
-    expression <- parseExpression
-    return $ PrintStatement expression
-
-parseLetStatement :: Parser Statement
-parseLetStatement = do
-    string "LET"
-    spaces
-    var <- parseVar
-    spaces
-    char '='
-    spaces
-    expression <- parseExpression
-    return $ LetStatement var expression
-
 parseStatement :: Parser Statement
 parseStatement = parsePrintStatement <|> parseLetStatement
+    where
+        parsePrintStatement :: Parser Statement
+        parsePrintStatement = do
+            string "PRINT"
+            spaces
+            expression <- parseExpression
+            return $ PrintStatement expression
+
+        parseLetStatement :: Parser Statement
+        parseLetStatement = do
+            string "LET"
+            spaces
+            var <- parseVar
+            spaces
+            char '='
+            spaces
+            expression <- parseExpression
+            return $ LetStatement var expression
 
 -- NUMBERS
 parseNumber :: Parser BasicNumber
